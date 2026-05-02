bd137c0efa35ebd53b3c0aea22739045260ba80c
Tom Furiolis <furiolis2005@gmail.com>
2026-05-02 17:16:20 +0200

    added Transfer model

--- transfers/models.py
+++ transfers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from .. banking import Account
+from datetime import date
 
-# Create your models here.
+class Transfer(models.Model):
+    sender = models.ForeignKey(Account)
+    recipient = models.ForeignKey(Account)
+    money = models.IntegerField()
+    date = models.DateField(auto_now=True, auto_now_add=False)
+    title = models.CharField(max_length=100)
+
+    
